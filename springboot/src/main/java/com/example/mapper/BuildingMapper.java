e80a095c7a2abbc0ca13d9c8c97f161fcac57992
zyx412 <2595851755@qq.com>
2025-04-24 08:34:34 +0800

    宿舍信息

---
+++ springboot/src/main/java/com/example/mapper/BuildingMapper.java
@@ -0,0 +1,37 @@
+package com.example.mapper;
+
+import com.example.entity.Building;
+
+import java.util.List;
+
+/**
+ * 操作building相关数据接口
+*/
+public interface BuildingMapper {
+
+    /**
+      * 新增
+    */
+    int insert(Building building);
+
+    /**
+      * 删除
+    */
+    int deleteById(Integer id);
+
+    /**
+      * 修改
+    */
+    int updateById(Building building);
+
+    /**
+      * 根据ID查询
+    */
+    Building selectById(Integer id);
+
+    /**
+      * 查询所有
+    */
+    List<Building> selectAll(Building building);
+
+}
