cc9093299ceb77296020c5537b0dfce46a63e932
mcyy-567 <2514775568@qq.com>
2025-04-26 16:05:06 +0800

    查询相关

---
+++ springboot/src/main/java/com/example/mapper/StayMapper.java
@@ -0,0 +1,38 @@
+package com.example.mapper;
+
+import com.example.entity.Notice;
+import com.example.entity.Stay;
+
+import java.util.List;
+
+/**
+ * 操作notice相关数据接口
+*/
+public interface StayMapper {
+
+    /**
+      * 新增
+    */
+    int insert(Stay notice);
+
+    /**
+      * 删除
+    */
+    int deleteById(Integer id);
+
+    /**
+      * 修改
+    */
+    int updateById(Stay notice);
+
+    /**
+      * 根据ID查询
+    */
+    Stay selectById(Integer id);
+
+    /**
+      * 查询所有
+    */
+    List<Stay> selectAll(Stay notice);
+
+}
