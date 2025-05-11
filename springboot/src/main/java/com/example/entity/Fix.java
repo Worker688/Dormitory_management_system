c75d4862a4303d6013ea12478efdbe69ac189559
ganyunnan <2337242545@qq.com>
2025-05-11 23:58:13 +0800

    寝室报修0.1

---
+++ springboot/src/main/java/com/example/entity/Fix.java
@@ -0,0 +1,72 @@
+package com.example.entity;
+
+import java.io.Serializable;
+
+/**
+ * 寝室报修表
+*/
+public class Fix implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private Integer studentId;
+
+    private Integer dormitoryId;
+
+    private String content;
+
+    private String img;
+
+    private String time;
+
+    private String status;
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public Integer getDormitoryId() {
+        return dormitoryId;
+    }
+
+    public void setDormitoryId(Integer dormitoryId) {
+        this.dormitoryId = dormitoryId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
